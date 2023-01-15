cd083cca46209f21b5e20d79418ae115e2a5baaa
mlbase <guddnr2424@gmail.com>
2023-01-15 16:15:24 +0900

    login 및 jwt entrypoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { ApiBasicAuth, ApiBearerAuth, ApiOAuth2 } from '@nestjs/swagger';
 import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
+import { JwtAuthGaurd } from './auth/jwt-auth.gaurd';
+import { LocalAuthGaurd } from './auth/local-auth.gaurd';
+import { UserLoginDto } from './users/dto/user-login.dto';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private authService: AuthService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @UseGuards(LocalAuthGaurd)
+  @Post('auth/login')
+  @ApiBasicAuth('user')
+  async login(@Body() user: UserLoginDto) {
+    return this.authService.login(user)
   }
+
+  @UseGuards(JwtAuthGaurd)
+  @Get('profile')
+  @ApiBearerAuth('user')
+  getProfile(@Request() req){
+    return req.user;
+  }
+
+
 }
