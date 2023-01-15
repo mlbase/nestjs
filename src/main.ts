b5388ebdf43d916d716a4ed54842c33ae60c7b4d
mlbase <guddnr2424@gmail.com>
2023-01-15 16:12:58 +0900

    hot reload 관련 의존성 추가

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+import { setupSwagger } from './swaggerSetup';
+
+declare const module: any
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const config = new DocumentBuilder()
-  .setTitle('swagger example')
-  .setDescription('swagger of api')
-  .setVersion('1.0')
-  .addTag('tutorial')
-  .build();
-  const document = SwaggerModule.createDocument(app, config)
-  SwaggerModule.setup('api', app, document)
-
+  setupSwagger(app);
   await app.listen(3000);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
